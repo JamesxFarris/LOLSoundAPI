83d9609c1b3caaa680026c38910ec762ab9c7668
James <jafarris.exe@gmail.com>
2025-11-12 16:35:00 -0500

    Added pseudocode to api

---
+++ server/server.js
@@ -0,0 +1,15 @@
+//Import modules
+import express from 'express';
+import cors from 'cors';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const app = express();
+//Front end connection
+app.use(cors());
+
+//Dirname fix
+
+//Routing
+
+//Serve files from the api
